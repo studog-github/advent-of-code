faad5b3d314e9c64aa9b146c51237e40cd02924b
Stuart MacDonald <stuartm.coding@gmail.com>
2018-11-30 11:27:20 -0500

    Make error messages clearer

--- blank.py
+++ blank.py
@@ -18,12 +18,12 @@ def main():
 
     input_path = os.path.abspath(args.file)
     if not os.path.exists(input_path):
-        print("%s: Error: '%s' not found" % (THIS, args.file))
+        print("%s: Error: File '%s' not found" % (THIS, args.file))
         return 1
 
     input = open(input_path, "r")
     if input is None:
-        print("%s: Error: Couldn't open '%s'" % (THIS, input_path))
+        print("%s: Error: Couldn't open file '%s'" % (THIS, input_path))
         return 1
 
     # Main loop
