8afd7036de743fbcf6f23f4d78f4adb4529727ca
Stuart MacDonald <stuartm.coding@gmail.com>
2020-12-01 21:28:22 -0500

    Add 2019's attempt

---
+++ 2019/computer.py
@@ -0,0 +1,154 @@
+class computer:
+    # Machine operations
+    def addition(self, mode):
+        num_instns = 4
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        r2 = self.program[self.ip + 2]
+        if mode[1] == '0':
+            r2 = self.program[r2]
+        s1 = self.program[self.ip + 3]
+        self.program[s1] = r1 + r2
+        self.ip += num_instns
+
+    def multiplication(self, mode):
+        num_instns = 4
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        r2 = self.program[self.ip + 2]
+        if mode[1] == '0':
+            r2 = self.program[r2]
+        s1 = self.program[self.ip + 3]
+        self.program[s1] = r1 * r2
+        self.ip += num_instns
+
+    def poke(self, mode):
+        num_instns = 2
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.poke_input()
+        #print("-= poke got %d" % r1)
+        s1 = self.program[self.ip + 1]
+        self.program[s1] = r1
+        self.ip += num_instns
+        
+    def peek(self, mode):
+        num_instns = 2
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        #print("-= peek returns %d" % r1)
+        self.peek_output(r1)
+        self.ip += num_instns
+        
+    def jumpiftrue(self, mode):
+        num_instns = 3
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        r2 = self.program[self.ip + 2]
+        if mode[1] == '0':
+            r2 = self.program[r2]
+        if r1 != 0:
+            self.ip = r2
+        else:
+            self.ip += num_instns
+
+    def jumpiffalse(self, mode):
+        num_instns = 3
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        r2 = self.program[self.ip + 2]
+        if mode[1] == '0':
+            r2 = self.program[r2]
+        if r1 == 0:
+            self.ip = r2
+        else:
+            self.ip += num_instns
+
+    def lessthan(self, mode):
+        num_instns = 4
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        r2 = self.program[self.ip + 2]
+        if mode[1] == '0':
+            r2 = self.program[r2]
+        s1 = self.program[self.ip + 3]
+        if r1 < r2:
+            self.program[s1] = 1
+        else:
+            self.program[s1] = 0
+        self.ip += num_instns
+
+    def equals(self, mode):
+        num_instns = 4
+        mode += '0' * (num_instns - len(mode))
+        r1 = self.program[self.ip + 1]
+        if mode[0] == '0':
+            r1 = self.program[r1]
+        r2 = self.program[self.ip + 2]
+        if mode[1] == '0':
+            r2 = self.program[r2]
+        s1 = self.program[self.ip + 3]
+        if r1 == r2:
+            self.program[s1] = 1
+        else:
+            self.program[s1] = 0
+        self.ip += num_instns
+
+    opcodes = {
+        1: addition,
+        2: multiplication,
+        3: poke,
+        4: peek,
+        5: jumpiftrue,
+        6: jumpiffalse,
+        7: lessthan,
+        8: equals,
+        99: None,
+    }
+
+    STATE_INITED = 0
+    STATE_RUNNING = 1
+    STATE_HALTED = 2
+
+    def poke_user_input(self):
+        return int(raw_input("poke needs a number: "))
+
+    def peek_user_output(self, v):
+        print("peek: %d" % (v))
+
+    # Regular members and methods
+    def __init__(self, program, fin=None, fout=None):
+        self.original_program = program
+        self.poke_input = fin if fin else self.poke_user_input
+        self.peek_output = fout if fout else self.peek_user_output
+
+    def reset(self):
+        self.program = list(self.original_program)
+        self.ip = 0
+        self.state = self.STATE_INITED
+
+    def run(self):
+        self.state = self.STATE_RUNNING
+        while True:
+            #print "program %s" % self.program
+            p_opcode = self.program[self.ip] % 100
+            p_opmode = str(self.program[self.ip])[:-2][::-1]
+            #print "ip: %d %s mode: '%s'" % (self.ip, self.program[self.ip:self.ip+4], p_opmode)
+            if p_opcode not in computer.opcodes:
+                print "ERROR: Unknown opcode %d at index %d, halted" % (p_opcode, self.ip)
+                return
+            elif computer.opcodes[p_opcode] is None:
+                self.state = self.STATE_HALTED
+                return
+            computer.opcodes[p_opcode](self, p_opmode)
