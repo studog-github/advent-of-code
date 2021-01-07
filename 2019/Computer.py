4c1b14fe3bf71fda1ccd18bf6594590c8b880d34
Stuart MacDonald <stuartm.coding@gmail.com>
2021-01-07 03:41:32 -0500

    [REFACTOR] Make the length of the opcode available
    Move the number of instructions from being hard-coded in each opcode routine to
    being part of the opcode dispatch table. This lets the main fetch decode run
    loop in run() print better debugging.

--- 2019/Computer.py
+++ 2019/Computer.py
@@ -9,7 +9,7 @@ class Computer:
     # Refactor parameter modes
 
     def addition(self, mode):
-        num_instns = 4
+        num_instns = self.opcodes[self.OP_ADD][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -26,7 +26,7 @@ class Computer:
         self.ip += num_instns
 
     def multiplication(self, mode):
-        num_instns = 4
+        num_instns = self.opcodes[self.OP_MULT][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -43,7 +43,7 @@ class Computer:
         self.ip += num_instns
 
     def poke(self, mode):
-        num_instns = 2
+        num_instns = self.opcodes[self.OP_POKE][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.poke_input()
         #print(f'-= poke got {r1}')
@@ -52,7 +52,7 @@ class Computer:
         self.ip += num_instns
         
     def peek(self, mode):
-        num_instns = 2
+        num_instns = self.opcodes[self.OP_PEEK][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -64,7 +64,7 @@ class Computer:
         self.ip += num_instns
         
     def jumpiftrue(self, mode):
-        num_instns = 3
+        num_instns = self.opcodes[self.OP_JMPT][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -82,7 +82,7 @@ class Computer:
             self.ip += num_instns
 
     def jumpiffalse(self, mode):
-        num_instns = 3
+        num_instns = self.opcodes[self.OP_JMPF][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -100,7 +100,7 @@ class Computer:
             self.ip += num_instns
 
     def lessthan(self, mode):
-        num_instns = 4
+        num_instns = self.opcodes[self.OP_LT][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -120,7 +120,7 @@ class Computer:
         self.ip += num_instns
 
     def equals(self, mode):
-        num_instns = 4
+        num_instns = self.opcodes[self.OP_EQ][self._OP_NINST]
         mode += '0' * (num_instns - 1 - len(mode))
         r1 = self.program[self.ip + 1]
         if mode[0] == '0':
@@ -140,7 +140,7 @@ class Computer:
         self.ip += num_instns
 
     def chrelbase(self, mode):
-        num_instns = 2
+        num_instns = self.opcodes[self.OP_CHRELB][self._OP_NINST]
         r1 = self.program[self.ip + 1]
         self.relbase += r1
         self.ip += num_instns
@@ -156,18 +156,20 @@ class Computer:
     OP_CHRELB = 9
     OP_HALT = 99
 
-    # Add num instructions here to improve debugging output
+    _OP_FN = 0
+    _OP_NINST = 1
+
     opcodes = {
-        OP_ADD: addition,
-        OP_MULT: multiplication,
-        OP_POKE: poke,
-        OP_PEEK: peek,
-        OP_JMPT: jumpiftrue,
-        OP_JMPF: jumpiffalse,
-        OP_LT: lessthan,
-        OP_EQ: equals,
-        OP_CHRELB: chrelbase,
-        OP_HALT: None,
+        OP_ADD: (addition, 4),
+        OP_MULT: (multiplication, 4),
+        OP_POKE: (poke, 2),
+        OP_PEEK: (peek, 2),
+        OP_JMPT: (jumpiftrue, 3),
+        OP_JMPF: (jumpiffalse, 3),
+        OP_LT: (lessthan, 4),
+        OP_EQ: (equals, 4),
+        OP_CHRELB: (chrelbase, 2),
+        OP_HALT: (None, 0),
     }
 
     STATE_INITED = 0
@@ -205,12 +207,12 @@ class Computer:
             print(f'program {self.program}')
             p_opcode = self.program[self.ip] % 100
             p_opmode = str(self.program[self.ip])[:-2][::-1]
-            print(f"ip: {self.ip} {self.program[self.ip:self.ip+4]} mode: '{p_opmode}' relbase: {self.relbase}")
+            print(f"ip: {self.ip} {self.program[self.ip:self.ip+self.opcodes[p_opcode][self._OP_NINST]]} mode: '{p_opmode}' relbase: {self.relbase}")
             if p_opcode not in self.opcodes:
                 print(f'ERROR: Unknown opcode {p_opcode} at index {self.ip}, halted')
                 self.state = self.STATE_HALTED
                 return
-            elif self.opcodes[p_opcode] is None:
+            elif self.opcodes[p_opcode][self._OP_FN] is None:
                 self.state = self.STATE_HALTED
                 return
-            self.opcodes[p_opcode](self, p_opmode)
+            self.opcodes[p_opcode][self._OP_FN](self, p_opmode)
