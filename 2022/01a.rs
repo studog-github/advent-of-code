a0c48d7d2175b7ebf8239dc6e6e13517cb401c6c
Stuart MacDonald <stuartm.coding@gmail.com>
2023-12-01 09:26:18 -0500

    First bit of rust
    Rust doesn't like global variables

--- 2022/01a.rs
+++ 2022/01a.rs
@@ -1,5 +1,16 @@
-static THIS: str = file!();
+use std::env;
+use once_cell::sync::Lazy;
+
+static THIS: &str = file!();
+static ARGS: Lazy<Vec<String>> = Lazy::new(|| env::args().collect());
+static THIS2: Lazy<&String> = Lazy::new(|| &ARGS[0]);
 
 fn main() {
-    println!("defined in file: {THIS}");
+    let args: Vec<String> = env::args().collect();
+
+    for (i, a) in args.iter().enumerate() {
+        println!("{i}: {a}")
+    }
+    let this: &str = file!();
+    println!("defined in file: [{this}] [{THIS}] [{}]", &*THIS2);
 }
