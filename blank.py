98e837714d6ae68ed8e5bc36b683a967a0e47c2b
Stuart MacDonald <stuartm.coding@gmail.com>
2024-12-01 01:23:47 -0500

    AoC 2024 python template updates
    - use env to locate python; this supports venvs etc
    - switch to single quotes where possible
    - fstrings not parameter formatting
    - add Counter from collections

--- blank.py
+++ blank.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import sys
 import os
@@ -8,26 +8,28 @@ from pprint import pprint
 from copy import deepcopy
 import itertools
 import math
+from collections import Counter
 
 THIS = os.path.basename(sys.argv[0])
 DEFAULT_INPUT = THIS + "-input"
 
 def main():
     arg_parser = argparse.ArgumentParser(prog=THIS)
-    arg_parser.add_argument("-f", "--file", default=DEFAULT_INPUT,
-                            help="specify the input file (default: %(default)s)", metavar="file")
+    arg_parser.add_argument('-f', '--file', default=DEFAULT_INPUT,
+                            help='specify the input file (default: %(default)s)', metavar='file')
     args = arg_parser.parse_args()
 
     # Open input file
 
     input_path = os.path.abspath(args.file)
     if not os.path.exists(input_path):
-        print("%s: Error: File '%s' not found" % (THIS, args.file))
+        print(f"{THIS}: Error: File '{args.file}' not found")
         return 1
 
-    input = open(input_path, "r")
-    if input is None:
-        print("%s: Error: Couldn't open file '%s'" % (THIS, input_path))
+    try:
+        input = open(input_path, 'r')
+    except:
+        print(f"{THIS}: Error: Couldn't open file '{input_path}'")
         return 1
 
     # Main loop
